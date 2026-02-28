365f03a8796226d9e1af284329a37fb452c7392b
vibhakar246 <vibhakarsingh246@gmail.com>
2026-03-01 02:11:52 +0530

    Fix Flask return syntax error

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "return "Hello from Flask DevOps 🚀""
+    return "Hello from Flask DevOps 🚀"
 
 @app.route('/health')
 def health():
